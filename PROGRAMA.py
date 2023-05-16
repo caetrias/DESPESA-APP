230bc14bd93e86e2cea8914e9bbb7fff0e7143aa
caetrias <gabrielcfarias77@gmail.com>
2023-05-16 14:29:38 -0300

    codigo implementado

--- PROGRAMA.py
+++ PROGRAMA.py
@@ -6,9 +6,13 @@ import time
 def clear():
     return os.system("cls")
 
+#TELA DO LOGIN
 def login():
+    clear()
     print("-------[LOGIN]-------")
-    login = input("Digite seu nome:")
+    print("INCOMPLETO")
+    time.sleep(1)
+    tela_despesas()
     #ABRIR ARQUIVO E VERIFICAR SE LOGIN ESTA DENTRO -> SE LOGIN NAO ESTIVER, EXIBIR MENSAGEM, ESPERAR E VOLTAR TELA INICIO
 
 #TELA CADASTRO
@@ -19,8 +23,8 @@ def cadastro():
     cadastros = open("cadastros.txt", "a")
     cadastros.write(nome)
     cadastros.close()
-
-    return 
+    tela_inicio()
+    
 #TELA INICIAL
 def tela_inicio():
     clear()
@@ -36,4 +40,36 @@ def tela_inicio():
     if decisao == 3:
         return print("FIM DE PROGRAMA")
 
+def tela_despesas():
+    clear()
+    print("-------------[DESPESAS]-------------")
+    print("[1]ADICIONAR GASTOS") 
+    print("[2]VISUALIZAR")
+    print("[3]APAGAR GASTOS")
+    print("[4]EDITAR")
+
+    decisao = int(input())
+    if decisao == 1:
+        return ADICIONAR_GASTOS()
+    if decisao == 2:
+        return VISUALIZAR_GASTOS()
+    if decisao == 3:
+        return APAGAR_GASTOS()
+    if decisao == 4:
+        return "tela edicao"
+
+def ADICIONAR_GASTOS():
+    print("") #-> input TITULO do gasto, CATEGORIA, VALOR
+
+def VISUALIZAR_GASTOS():
+    print("") #-> read no arquivo CSV/dicionario
+              #  -> PERGUNTA: CATEGORIA ESPECIFICA; PERGUNTA: GASTOS POR CATEGORIA
+
+def APAGAR_GASTOS():
+    print("") #-> ESCOLHE CATEGORIA -> exibe lista (ex.: brinquedo:10) -> PERGUNTA: limpar categoria
+              #                                                        -> PERGUNTA: apagar um titulo
+              #                                                        -> PERGUNTA: apagar um valor do titulo
+
+def EDITAR():
+    print("")
 tela_inicio()
